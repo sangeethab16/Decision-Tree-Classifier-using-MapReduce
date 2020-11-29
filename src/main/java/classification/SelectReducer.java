564e1ac7382e02c62072251ae08ce633d0ee8c16
Prajakta Rodrigues <prajaktarodrigues@gmail.com>
2020-11-28 17:58:07 -0800

    Reduce function updated

--- src/main/java/classification/SelectReducer.java
+++ src/main/java/classification/SelectReducer.java
@@ -1,6 +1,7 @@
 package classification;
 
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -8,16 +9,23 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 //input is of the form key = Ak, [(Xj/Ak, CutPoint Xj), (Xj+1/Ak, CutPoint Xj+1), (Xj+2/Ak, CutPoint Xj+2)]
-public class SelectReducer extends Reducer<IntWritable, SelectMapperWritable, IntWritable, DoubleWritable> {
-
+public class SelectReducer extends Reducer<IntWritable, SelectMapperWritable, IntWritable, SelectMapperWritable> {
+    
     @Override
     public void reduce(final IntWritable key, final Iterable<SelectMapperWritable> values, final Context context) throws IOException, InterruptedException {
-        int sum = 0;
-//        for (final IntWritable val : values) {
-//            sum += val.get();
-//        }
-//        result.set(sum);
-//        context.write(key, result);
+        float ratio = 0.0f;
+        int totalValueCount = 0;
+        double cutPointSum = 0.0;
+
+        for (final SelectMapperWritable val : values) {
+            ratio += val.getRatio().get();
+            cutPointSum += val.getAttributeValue().get();
+            totalValueCount += 1;
+        }
+
+        double cutPointAvg = cutPointSum / totalValueCount ;
+
+        context.write(key, new SelectMapperWritable(new FloatWritable(ratio), new DoubleWritable(cutPointAvg)));
     }
 
 }
