efeafb45909dfd8aa86b7146930c3a2a2d7e5423
Maanasa Kaza <maanasakaza@gmail.com>
2020-11-28 23:58:16 -0500

    Code for adding probabilities for class labels to calculate entropy

--- src/main/java/classification/AttributeSelectionMapper.java
+++ src/main/java/classification/AttributeSelectionMapper.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -58,6 +61,41 @@ public class AttributeSelectionMapper extends Mapper<LongWritable, Text, Text, T
 	
 	@Override
 	public void cleanup(Context context) {
+		
+		Float infoDataset = (float) 0.0;
+		
+		List<InstanceValue> classLabelValues = attributeMetrics.get(0);
+		List<String> classLabels = new ArrayList<String>();
+		
+		for(int i = 0; i < classLabelValues.size(); i++) {
+			
+			classLabels.add(classLabelValues.get(i).getClassLabel());
+		}
+		
+		Set<String> uniqueClassLabels = new HashSet<String>(classLabels);
+		Map<String, Float> classProbability = new HashMap<String, Float>();
+		
+		for (Iterator<String> iter = uniqueClassLabels.iterator(); iter.hasNext(); ) {
+			classProbability.put(iter.next(), (float)0.0);
+	    }
+		
+		
+		
+		for(int i = 0; i < classLabels.size(); i++) {
+			Float count = classProbability.get(classLabels.get(i));
+			count++;
+			classProbability.put(classLabels.get(i), count);
+		}
+		
+		for (Iterator<String> iter = uniqueClassLabels.iterator(); iter.hasNext(); ) {
+			String classLabel = iter.next();
+			classProbability.put(classLabel, classProbability.get(classLabel)/classLabels.size());
+	    }
+		
+		//System.out.println("CLASS PROBABILITY : " + classProbability.toString());
+		
+		
+		
 		for(Map.Entry<Integer, List<InstanceValue>> entry : attributeMetrics.entrySet()) {
 			//System.out.println("Attribute ID : " + entry.getKey());
 			//System.out.println("Attribute Value List : " + entry.getValue().toString());
@@ -69,12 +107,14 @@ public class AttributeSelectionMapper extends Mapper<LongWritable, Text, Text, T
 				cpList.add(Double.sum(attrValList.get(i).getValue(), attrValList.get(i+1).getValue())/2);
 			}
 			
-			System.out.println("CUT POINT : " + entry.getKey() + " " + cpList.toString());
-			
-			
-			
+			//System.out.println("CUT POINT : " + entry.getKey() + " " + cpList.toString());
+			/*for(int i = 0; i< cpList.size(); i++) {
+				
+			}*/
 			
 		}
+		
+		
 			
 		
 	}
