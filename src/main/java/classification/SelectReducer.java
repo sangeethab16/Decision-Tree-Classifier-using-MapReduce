aeba7215d3dd2d017e7d17b56404b16e9a75c0de
Prajakta Rodrigues <prajaktarodrigues@gmail.com>
2020-11-28 18:18:57 -0800

    Reduce function completed

--- src/main/java/classification/SelectReducer.java
+++ src/main/java/classification/SelectReducer.java
@@ -1,5 +1,6 @@
 package classification;
 
+import classification.utility.RatioCutPoint;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
@@ -7,10 +8,14 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 //input is of the form key = Ak, [(Xj/Ak, CutPoint Xj), (Xj+1/Ak, CutPoint Xj+1), (Xj+2/Ak, CutPoint Xj+2)]
-public class SelectReducer extends Reducer<IntWritable, SelectMapperWritable, IntWritable, SelectMapperWritable> {
-    
+public class SelectReducer extends Reducer<IntWritable, SelectMapperWritable, IntWritable, DoubleWritable> {
+
+    Map<Integer, RatioCutPoint> ratioCutPointMap = new HashMap<>();
+
     @Override
     public void reduce(final IntWritable key, final Iterable<SelectMapperWritable> values, final Context context) throws IOException, InterruptedException {
         float ratio = 0.0f;
@@ -25,7 +30,26 @@ public class SelectReducer extends Reducer<IntWritable, SelectMapperWritable, In
 
         double cutPointAvg = cutPointSum / totalValueCount ;
 
-        context.write(key, new SelectMapperWritable(new FloatWritable(ratio), new DoubleWritable(cutPointAvg)));
+        ratioCutPointMap.put(key.get(), new RatioCutPoint(ratio, cutPointAvg));
+    }
+
+    @Override
+    public void cleanup(final Context context) throws IOException, InterruptedException {
+        float maxRatio = Float.MIN_VALUE;
+        int selectedAttribute = -1;
+        double selectedAttributeCutPoint = 0.0;
+
+        for(Map.Entry<Integer, RatioCutPoint> ratioCutPointEntry : ratioCutPointMap.entrySet()) {
+            if(ratioCutPointEntry.getValue().getRatio() > maxRatio) {
+                selectedAttribute = ratioCutPointEntry.getKey();
+                selectedAttributeCutPoint = ratioCutPointEntry.getValue().getCutPoint();
+                maxRatio = ratioCutPointEntry.getValue().getRatio();
+            }
+        }
+        IntWritable key = new IntWritable(selectedAttribute);
+        DoubleWritable value = new DoubleWritable(selectedAttributeCutPoint);
+
+        context.write(key, value);
     }
 
 }
