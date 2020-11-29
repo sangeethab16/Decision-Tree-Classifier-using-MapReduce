fba8e34fabd404b5d9318354281e1bc71a1458f4
Maanasa Kaza <maanasakaza@gmail.com>
2020-11-28 20:43:58 -0500

    Add custom class

---
+++ src/main/java/classification/SelectMapperWritable.java
@@ -0,0 +1,54 @@
+package classification;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class SelectMapperWritable implements Writable {
+
+    private FloatWritable ratio;
+    private DoubleWritable attributeValue;
+
+    public SelectMapperWritable(FloatWritable ratio, DoubleWritable attributeValue) {
+        this.ratio = ratio;
+        this.attributeValue = attributeValue;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        ratio.write(out);
+        attributeValue.write(out);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        ratio.readFields(in);
+        attributeValue.readFields(in);
+    }
+
+    public FloatWritable getRatio() {
+        return ratio;
+    }
+
+    public void setRatio(FloatWritable ratio) {
+        this.ratio = ratio;
+    }
+
+    public DoubleWritable getAttributeValue() {
+        return attributeValue;
+    }
+
+    public void setAttributeValue(DoubleWritable attributeValue) {
+        this.attributeValue = attributeValue;
+    }
+
+
+    @Override
+    public String toString() {
+        return ratio + "," + attributeValue;
+    }
+}
