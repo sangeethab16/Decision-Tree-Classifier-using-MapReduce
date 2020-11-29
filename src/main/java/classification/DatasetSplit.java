f96bd00c8a9f0f7b2f3be005499e7ebd11cec6ef
Bhagavatula <bhagavatula.s@northeastern.edu>
2020-11-29 00:34:46 -0500

    Implemented mapper

--- src/main/java/classification/DatasetSplit.java
+++ src/main/java/classification/DatasetSplit.java
@@ -1,11 +1,14 @@
 package classification;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -14,46 +17,60 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-public class DataPreparation extends Configured implements Tool {
-	private static final Logger logger = LogManager.getLogger(DataPreparation.class);
+public class DatasetSplit extends Configured implements Tool {
+	private static final Logger logger = LogManager.getLogger(DatasetSplit.class);
 
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable(1);
-		private final Text word = new Text();
+	public static class SplitMapper extends Mapper<Object, Text, IntWritable, Text> {
+		private final IntWritable result = new IntWritable();
+		int ak;
+		double cpk;
 
 		@Override
-		public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
-			final StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				context.write(word, one);
-			}
+		public void setup(Context context) {
+			ak = Integer.parseInt(context.getConfiguration().get("ak"));
+			cpk = Double.parseDouble(context.getConfiguration().get("cpk"));
 		}
-	}
+		@Override
+		public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
 
-	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+			String[] row = value.toString().split(",");
+			float[] rowValues = new float[row.length];
+			for(int i=0;i< row.length; i++)
+				rowValues[i] = float.parseFloat(row[i]);
+			int id = 0;
+			if(rowValues[ak] > cpk)
+				id = 1;
+			else
+				id = 0;
+			context.write(new IntWritable(id),value);
+			}
+
+		}
+	public static class SplitReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
 		private final IntWritable result = new IntWritable();
 
 		@Override
-		public void reduce(final Text key, final Iterable<IntWritable> values, final Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (final IntWritable val : values) {
-				sum += val.get();
-			}
-			result.set(sum);
-			context.write(key, result);
+		public void reduce(final IntWritable key, final Iterable<Text> values, final Context context) throws IOException, InterruptedException {
+			int k = 2;
+			float[] prob = new float[k];
+			int n = 0;
+			for( Text val: values)
+				n += 1;
+			// Should implement the algorithm
+
+			result.set();
+			context.write(key, new Text());
 		}
-	}
+
 
 	@Override
 	public int run(final String[] args) throws Exception {
 		final Configuration conf = getConf();
 		final Job job = Job.getInstance(conf, "Word Count");
-		job.setJarByClass(DataPreparation.class);
+		job.setJarByClass(DatasetSplit.class);
 		final Configuration jobConf = job.getConfiguration();
 		jobConf.set("mapreduce.output.textoutputformat.separator", "\t");
 		// Delete output directory, only to ease local development; will not work on AWS. ===========
@@ -62,26 +79,14 @@ public class DataPreparation extends Configured implements Tool {
 //			fileSystem.delete(new Path(args[1]), true);
 //		}
 		// ================
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
+		job.setMapperClass(ComputeMapper.class);
+		job.setCombinerClass(ComputeReducer.class);
+		job.setReducerClass(ComputeReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		return job.waitForCompletion(true) ? 0 : 1;
-	}
-
-	public static void main(final String[] args) {
-		if (args.length != 2) {
-			throw new Error("Two arguments required:\n<input-dir> <output-dir>");
-		}
-
-		try {
-			ToolRunner.run(new DataPreparation(), args);
-		} catch (final Exception e) {
-			logger.error("", e);
-		}
 	}
 
 }
