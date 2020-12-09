f83687ab1af09ac5de2e8eda4862fb4503630c12
Prajakta Rodrigues <prajaktarodrigues@gmail.com>
2020-12-09 12:55:25 -0800

    Test commit

--- src/main/java/classification/DatasetSplit.java
+++ src/main/java/classification/DatasetSplit.java
@@ -45,23 +45,6 @@ public class DatasetSplit extends Configured implements Tool {
 
 		}
 
-	public static class SplitReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
-		private final IntWritable result = new IntWritable();
-
-		@Override
-		public void reduce(final IntWritable key, final Iterable<Text> values, final Context context) throws IOException, InterruptedException {
-			int k = 2;
-			float[] prob = new float[k];
-			int n = 0;
-			for (Text val : values)
-				n += 1;
-			// Algorithm to be implemented
-
-			context.write(key, new Text());
-		}
-
-	}
-
 	@Override
 	public int run(final String[] args) throws Exception {
 		final Configuration conf = getConf();
