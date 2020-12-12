8123d495f49202ecf94bc4c3657c3fa8d99e9b87
Prajakta Rodrigues <prajaktarodrigues@gmail.com>
2020-12-12 01:22:20 -0800

    Driver updated

--- src/main/java/classification/Driver.java
+++ src/main/java/classification/Driver.java
@@ -2,16 +2,21 @@ package classification;
 
 import java.io.Console;
 import java.io.IOException;
+
+import classification.utility.SPLIT_COUNTER;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.LogManager;
@@ -22,20 +27,76 @@ public class Driver extends Configured implements Tool {
 
 	@Override
 	public int run(final String[] args) throws Exception {
-		final Configuration conf = getConf();
-		final Job job = Job.getInstance(conf, "Word Count");
-		job.setJarByClass(Driver.class);
-		final Configuration jobConf = job.getConfiguration();
-		jobConf.set("mapreduce.output.textoutputformat.separator", ",");
-		jobConf.setLong("selectedAttribute", 0);
-		jobConf.setDouble("selectedAttributeCutPoint", 0);
-		job.setMapperClass(AttributeSelectionMapper.class);
-		job.setReducerClass(SelectReducer.class);
-		job.setOutputKeyClass(IntWritable.class);
-		job.setOutputValueClass(SelectMapperWritable.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		job.waitForCompletion(true);
+
+		int maxHeight = 1;
+		int height = 0;
+
+		Path inputPath = new Path(args[0]);
+		Path outputPath = new Path(args[1] + "/0");
+
+
+		while(height < maxHeight) {
+			Configuration conf = getConf();
+			Job job = Job.getInstance(conf, "Decision Tree");
+			job.setJarByClass(Driver.class);
+			final Configuration jobConf = job.getConfiguration();
+			jobConf.set("mapreduce.output.textoutputformat.separator", ",");
+
+			job.setMapperClass(AttributeSelectionMapper.class);
+			job.setReducerClass(SelectReducer.class);
+
+
+			job.setOutputKeyClass(IntWritable.class);
+			job.setOutputValueClass(SelectMapperWritable.class);
+
+			FileInputFormat.addInputPath(job, inputPath);
+			FileOutputFormat.setOutputPath(job, new Path("tempo"));
+
+			job.waitForCompletion(true);
+
+			long cutpoint = job.getCounters().findCounter(SPLIT_COUNTER.CUTPOINT).getValue();
+			double finalCutpoint = (double) cutpoint / 100000;
+			System.out.println(finalCutpoint);
+
+			long selectedAttribute = job.getCounters().findCounter(SPLIT_COUNTER.ATTRIBUTE_COLUMN).getValue();
+			System.out.println(selectedAttribute);
+
+
+
+
+
+			Configuration confTwo = getConf();
+			Job jobTwo = Job.getInstance(confTwo, "Decision Tree Building");
+			jobTwo.setJarByClass(Driver.class);
+			Configuration jobConfigTwo = jobTwo.getConfiguration();
+			jobConfigTwo.set("mapreduce.output.textoutputformat.separator", ",");
+
+			jobConfigTwo.setLong("selectedAttribute", selectedAttribute);
+			jobConfigTwo.setDouble("selectedAttributeCutPoint", finalCutpoint);
+			jobConfigTwo.setFloat("minProbability", 0.90f);
+			jobConfigTwo.setInt("maxRecordsInPartition", 2);
+
+			jobTwo.setMapperClass(SplitMapper.class);
+			jobTwo.setReducerClass(SplitReducer.class);
+
+			FileInputFormat.addInputPath(jobTwo, inputPath);
+			FileOutputFormat.setOutputPath(jobTwo, outputPath);
+
+			MultipleOutputs.addNamedOutput(jobTwo, "tree", TextOutputFormat.class, LongWritable.class, Text.class);
+
+			MultipleOutputs.addNamedOutput(jobTwo, "data", TextOutputFormat.class, Text.class, Text.class);
+
+			jobTwo.waitForCompletion(true);
+
+
+
+			height++;
+
+		}
+
+
+
+
 		return 1;
 	}
 
