8375d7fe20809a5459d702e5ad8ef6fcdbfa2448
Anugraha Venugopal <44260825+anu-venu@users.noreply.github.com>
2020-12-12 05:00:56 -0500

    Update Driver.java
    Correction in code.

--- src/main/java/classification/Driver.java
+++ src/main/java/classification/Driver.java
@@ -21,22 +21,11 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.LogManager;
 
+public class Driver extends Configured implements Tool {
+	private static final Logger logger = LogManager.getLogger(Driver.class);
+	
 	@Override
 	public int run(final String[] args) throws Exception {
-		final Configuration conf = getConf();
-		final Job job = Job.getInstance(conf, "Word Count");
-		job.setJarByClass(Driver.class);
-		final Configuration jobConf = job.getConfiguration();
-		jobConf.set("mapreduce.output.textoutputformat.separator", ",");
-		jobConf.setLong("selectedAttribute", 0);
-		jobConf.setDouble("selectedAttributeCutPoint", 0);
-		job.setMapperClass(AttributeSelectionMapper.class);
-		job.setReducerClass(SelectReducer.class);
-		job.setOutputKeyClass(IntWritable.class);
-		job.setOutputValueClass(SelectMapperWritable.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		job.waitForCompletion(true);
 
 		int maxHeight = 1;
 		int height = 0;
@@ -99,4 +88,15 @@ import org.apache.log4j.LogManager;
 		}
 		return 1;
 	}
+	public static void main(final String[] args) {
+		if (args.length != 2) {
+			throw new Error("Two arguments required:\n<input-dir> <output-dir>");
+		}
 
+		try {
+			ToolRunner.run(new Driver(), args);
+		} catch (final Exception e) {
+			logger.error("", e);
+		}
+	}
+}
