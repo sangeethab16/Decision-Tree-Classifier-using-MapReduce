4a2e25f5af2ddb6d41e116700f2192f8f6360c2c
Anugraha Venugopal <44260825+anu-venu@users.noreply.github.com>
2020-12-13 01:27:03 -0500

    Update dataDiffSizes.java

--- src/main/java/classification/dataDiffSizes.java
+++ src/main/java/classification/dataDiffSizes.java
@@ -1,12 +1,15 @@
 //obtain random subsets of the data of different sizes for running experiments with on AWS
+package classification; 
 
 import java.io.IOException;
 import java.util.Random;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class DataDiffSizes extends Mapper<LongWritable, Text, IntWritable, SelectMapperWritable>{
+public class DataDiffSizes extends Mapper<Object, Text, LongWritable, Text>{
 	private Random randomNumber = new Random();
 	private Double samplingPercentage;
 	protected void setup(Context context) throws IOException, InterruptedException {
