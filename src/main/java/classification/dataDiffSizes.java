5bd304a53fd0b86d170cdbb400ac9e5a8a10f4e3
Anugraha Venugopal <44260825+anu-venu@users.noreply.github.com>
2020-12-13 01:39:50 -0500

    Random sampling compared with threshold

--- src/main/java/classification/dataDiffSizes.java
+++ src/main/java/classification/dataDiffSizes.java
@@ -9,11 +9,21 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class DataDiffSizes extends Mapper<Object, Text, LongWritable, Text>{
+public class DataDiffSizes extends Mapper<Object, Text, LongWritable, NullWritableText>{
 	private Random randomNumber = new Random();
 	private Double samplingPercentage;
+	
+	//calculates sampling percentage to use as threshold
 	protected void setup(Context context) throws IOException, InterruptedException {
 		String percentage = context.getConfiguration().get("sampling_percentage");
 		samplingPercentage = Double.parseDouble(percentage) / 100.0;
+	}
+	
+	@Override
+	//compares randomly generated number with threshold to decide whether to keep value or not
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		if (randomNumber.nextDouble() < samplingPercentage) {
+			context.write(NullWritable.get(), value);
+		}
 	}
 }
