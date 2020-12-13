b2856b48c5479dba88d4589b9c6423b241b8df26
Anugraha Venugopal <44260825+anu-venu@users.noreply.github.com>
2020-12-13 17:27:40 -0500

    Comments & formatting

--- src/main/java/classification/AccuracyReducer.java
+++ src/main/java/classification/AccuracyReducer.java
@@ -6,14 +6,11 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-
-
-
 public class AccuracyReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
 	
 	@Override
 	public void reduce(final IntWritable key, final Iterable<IntWritable> values, final Context context) throws IOException, InterruptedException {
-		
+		//check if key’s value indicates signal (1) or noise (0/not 1) and increment TRUE counter accordingly
 		for (final IntWritable val : values) {
 			context.getCounter(CounterEnum.TOTAL).increment(1);
 			if (key.get() == 1) {
@@ -23,4 +20,3 @@ public class AccuracyReducer extends Reducer<IntWritable, IntWritable, IntWritab
 	}
 	
 }
-
